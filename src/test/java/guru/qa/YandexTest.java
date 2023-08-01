01167fa1c96bfaddcfa9a6945a71f12a6f281817
Vladimir <none@none>
2023-08-01 19:31:13 -0400

    empty yandex test

---
+++ src/test/java/guru/qa/YandexTest.java
@@ -0,0 +1,15 @@
+package guru.qa;
+
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class YandexTest {
+
+    @Test
+    void yandexTest() {
+
+    }
+}
